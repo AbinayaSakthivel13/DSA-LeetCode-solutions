6d20c69472c72e6de7bb7a2579dafb8aec9b00f2
Abinaya S <abinayasakthivel61@gmail.com>
2024-12-20 21:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.22%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,17 +4,13 @@ public:
         if(x<0)
             return false;
         int copy=x;
-        vector<int> arr;
-        while(copy>0){
-            arr.push_back(copy%10);
+        int res=0;
+        while(copy){
+            if(res>INT_MAX/10 || (res==INT_MAX/10 && copy%10>7))
+                return false;
+            res = res*10 + copy%10;
             copy=copy/10;
         }
-        int left=0,right=arr.size()-1;
-        while(left<right){
-            if(arr[left]!=arr[right])
-                return false;
-            left++; right--;
-        }
-        return true;
+        return x==res;
     }
 };
