984b5a1d05346a38e34630b93f0572a77dde208e
Abinaya S <abinayasakthivel61@gmail.com>
2024-12-20 21:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (11.54%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -4,10 +4,10 @@ public:
         int copy=x;
         int res=0,rem=0;
         while(copy!=0){
-            rem=copy%10;
-            if(res>INT_MAX/10 || (res==INT_MAX/10 && rem>7))
-                return 0;
+            rem = copy%10;
             if(res<INT_MIN/10 || (res==INT_MIN/10 && rem<-8))
+                return 0;
+            if(res>INT_MAX/10 || (res==INT_MIN/10 && rem>7))
                 return 0;
             res=res*10+rem;
             copy=copy/10;
