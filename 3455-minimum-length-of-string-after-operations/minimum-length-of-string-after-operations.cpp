09f542131e110dda979c9b78296b8c5fd8038497
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-13 21:21:22 +0530

    Time: 9 ms (82.26%) | Memory: 31.9 MB (93.55%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        vector<int> charFrequency(26, 0);
-        int totalLength = 0;
-        for (char currentChar : s) {
-            charFrequency[currentChar - 'a']++;
+        vector<int> cnt(26, 0);
+        int len = 0;
+        
+        for (char ch : s) {
+            cnt[ch - 'a']++;
         }
-        for (int frequency : charFrequency) {
-            if (frequency == 0) continue;
-            if (frequency % 2 == 0) {
-                totalLength += 2;
-            } else {
-                totalLength += 1;
-            }
-        }
-        return totalLength;
+        
+        for (int num : cnt) {
+            if (num == 0) continue;
+            len += (num % 2 == 0) ? 2 : 1;
+        }        
+        return len;
     }
 };
