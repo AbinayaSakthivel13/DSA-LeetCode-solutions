238c5555dff1e48f018c76e362fd13aeacc6f207
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-18 22:51:54 +0530

    Time: 1 ms (99.16%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,33 +1,53 @@
-class Solution {
- public:
-  int minCost(vector<vector<int>>& grid) {
-    const int m = grid.size();
-    const int n = grid[0].size();
-    vector<vector<int>> mem(m, vector<int>(n, -1));
-    queue<pair<int, int>> q;
-    dfs(grid, 0, 0, /*cost=*/0, q, mem);
-    for (int cost = 1; !q.empty(); ++cost)
-      for (int sz = q.size(); sz > 0; --sz) {
-        const auto [i, j] = q.front();
-        q.pop();
-        for (const auto& [dx, dy] : dirs)
-          dfs(grid, i + dx, j + dy, cost, q, mem);
-      }
-    return mem.back().back();
-  }
+typedef unsigned char utype;
+typedef pair<utype, utype> pcc;
+#define ff first
+#define ss second
 
- private:
-  static constexpr int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-  void dfs(const vector<vector<int>>& grid, int i, int j, int cost,
-           queue<pair<int, int>>& q, vector<vector<int>>& mem) {
-    if (i < 0 || i == grid.size() || j < 0 || j == grid[0].size())
-      return;
-    if (mem[i][j] != -1){
-      return;
+class Solution {
+public:
+    const utype di[4] = { 0, 0, 1, static_cast<utype>(-1) };
+    const utype dj[4] = { 1, static_cast<utype>(-1), 0, 0 };
+    utype vis[100][100];
+    int minCost(vector<vector<int>>& grid) {
+        utype m = grid.size();
+        utype n = grid[0].size();
+        memset(vis, -1, sizeof(vis));
+
+        std::vector<pcc> q, nq;
+        q.reserve(1024);
+        nq.reserve(1024);
+        int c = 0;
+
+        q.emplace_back(0, 0);
+        vis[0][0] = 0;
+
+        while (!q.empty()) {
+            while (!q.empty()) {
+                pcc x = q.back(); q.pop_back();
+                if (vis[x.ff][x.ss] != c) continue;
+                if (x.ff == m - 1 && x.ss == n - 1) return c;
+                for (utype k = 0, ni, nj; k < 4; ++k) {
+                    ni = x.ff + di[k];
+                    nj = x.ss + dj[k];
+                    if (m <= ni || n <= nj) continue;
+
+                    if (grid[x.ff][x.ss] - 1 == k) {
+                        if (vis[ni][nj] > c) {
+                            vis[ni][nj] = c;
+                            q.emplace_back(ni, nj);
+                        }
+                    }
+                    else {
+                        if (vis[ni][nj] > c + 1) {
+                            vis[ni][nj] = c + 1;
+                            nq.emplace_back(ni, nj);
+                        }
+                    }
+                }
+            }
+            q.swap(nq);
+            c += 1;
+        }
+        return -1;
     }
-    mem[i][j] = cost;
-    q.emplace(i, j);
-    const auto& [dx, dy] = dirs[grid[i][j] - 1];
-    dfs(grid, i + dx, j + dy, cost, q, mem);
-  }
 };
