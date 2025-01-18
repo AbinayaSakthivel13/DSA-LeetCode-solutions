3db5635d3a82b2ca58e1f38056d0a434bb60e371
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-18 22:50:40 +0530

    Time: 7 ms (96.20%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,35 +1,64 @@
+const int di[4] = {0, 0, 1, -1};
+const int dj[4] = {1, -1, 0, 0};
+const int N = 10000;
 class Solution {
 public:
-    vector<int> xd = {0, 0, 1, -1};
-    vector<int> yd = {1, -1, 0, 0};
-    int minCost(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        set<tuple<int, int, int>> s;
-        s.insert({0, 0, 0});
+    static inline bool isOutside(int i, int j, int r, int c) {
+        return i < 0 || i >= r || j < 0 || j >= c;
+    }
+    static inline unsigned pack(unsigned d, unsigned i, unsigned j) {
+        return (d << 16) + (i << 8) + j;
+    }
+    static inline array<int, 3> unpack(unsigned info) {
+        array<int, 3> ans;
+        ans[0] = info >> 16, ans[1] = (info >> 8) & 255, ans[2] = info & 255;
+        return ans;
+    }
+    static unsigned int idx(int i, int j, int c) { return i * c + j; }
+    static int minCost(vector<vector<int>>& grid) {
+        const int r = grid.size(), c = grid[0].size();
+        unsigned q[N];
+        int front=0, back=0;
+        unsigned* dist = (unsigned*)alloca(r * c * sizeof(unsigned));
+        bitset<N> viz = 0;
+        fill(dist, dist + r * c, UINT_MAX);
+        q[back++]=pack(0, 0, 0);
+        dist[0] = 0;
+        viz[0] = 1;
+        while (front!=back) {
+            auto info = q[front++];
+            if (front>=N) front-=N;
 
-        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
-        dis[0][0] = 0;
-        while (!s.empty()) {
-            auto [cost, x, y] = *(s.begin());
-            s.erase(s.begin());
-            if (x == n - 1 && y == m - 1) {
-                return cost;
-            }
-            int p = grid[x][y];
-            for (int i = 1; i <= 4; i++) {
-                if (x + xd[i - 1] >= 0 && x + xd[i - 1] < n &&
-                    y + yd[i - 1] >= 0 && y + yd[i - 1] < m) {
-                    if (!(p == i) + dis[x][y] <
-                        dis[x + xd[i - 1]][y + yd[i - 1]]) {
-                        dis[x + xd[i - 1]][y + yd[i - 1]] =
-                            !(p == i) + dis[x][y];
-                        s.insert({dis[x + xd[i - 1]][y + yd[i - 1]],
-                                  x + xd[i - 1], y + yd[i - 1]});
+            auto [d, i, j] = unpack(info);
+            viz[idx(i, j, c)] = 1;
+            if (i == r - 1 && j == c - 1)
+                return d;
+            int x = grid[i][j];
+            for (int a = 0; a < 4; a++) {
+                int s = i + di[a], t = j + dj[a];
+                if (isOutside(s, t, r, c) || viz[idx(s, t, c)])
+                    continue;
+                int new_d = d + 1 - (a + 1 == x);
+                int b = idx(s, t, c);
+                if (new_d < dist[b]) {
+                    dist[b] = new_d;
+                    if (a+1==x){
+                        if (front==0) front=N;
+                        q[--front]=pack(new_d, s, t);
+                    }
+                    else{
+                        q[back++]=pack(new_d, s, t);
+                        if (back>=N) back-=N;
                     }
                 }
             }
         }
-        return 0;
+        return INT_MAX;
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
