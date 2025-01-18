9636948b17e363c8836793fa120b89cf56814967
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-18 22:48:46 +0530

    Time: 15 ms (78.06%) | Memory: 26.2 MB (38.12%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,49 +1,33 @@
-const int di[4] = {0, 0, 1, -1};
-const int dj[4] = {1, -1, 0, 0};
+#include <bits/stdc++.h>
+using namespace std;
+const int dx[4] = {0, 0, 1, -1};
+const int dy[4] = {1, -1, 0, 0};
 class Solution {
 public:
-    static inline bool isOutside(int i, int j, int r, int c) {
-        return i < 0 || i >= r || j < 0 || j >= c;
-    }
-    static inline unsigned pack(unsigned d, unsigned i, unsigned j) {
-        return (d << 16) + (i << 8) + j;
-    }
-    static inline array<int, 3> unpack(unsigned info) {
-        array<int, 3> ans;
-        ans[0] = info >> 16, ans[1] = (info >> 8) & 255, ans[2] = info & 255;
-        return ans;
-    }
-    static unsigned int idx(int i, int j, int c) { return i * c + j; }
-
-    static int minCost(vector<vector<int>>& grid) {
-        const int r = grid.size(), c = grid[0].size();
-        priority_queue<unsigned, vector<unsigned>, greater<>> pq;
-        unsigned* dist = (unsigned*)alloca(r * c * sizeof(unsigned));
-        bitset<10000> viz = 0;
-        fill(dist, dist + r * c, UINT_MAX);
-        pq.push(pack(0, 0, 0));
-        dist[0] = 0;
-        viz[0] = 1;
-        while (!pq.empty()) {
-            auto info = pq.top();
-            pq.pop();
-            auto [d, i, j] = unpack(info);
-            viz[idx(i, j, c)] = 1;
-            if (i == r - 1 && j == c - 1)
-                return d;
-            int x = grid[i][j];
-            for (int a = 0; a < 4; a++) {
-                int s = i + di[a], t = j + dj[a];
-                if (isOutside(s, t, r, c) || viz[idx(s, t, c)])
-                    continue;
-                int new_d = d + 1 - (a + 1 == x);
-                int b = idx(s, t, c);
-                if (new_d < dist[b]) {
-                    dist[b] = new_d;
-                    pq.push(pack(new_d, s, t));
+    int minCost(vector<vector<int>>& grid) {
+        int r = grid.size(), c = grid[0].size();
+        vector<vector<int>> dist(r, vector<int>(c, INT_MAX));
+        deque<pair<int, int>> dq;
+        dq.emplace_front(0, 0);
+        dist[0][0] = 0;
+        
+        while (!dq.empty()) {
+            auto [x, y] = dq.front(); dq.pop_front();
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
+                    int cost = (i + 1 == grid[x][y]) ? 0 : 1;
+                    if (dist[x][y] + cost < dist[nx][ny]) {
+                        dist[nx][ny] = dist[x][y] + cost;
+                        if (cost == 0) {
+                            dq.emplace_front(nx, ny);
+                        } else {
+                            dq.emplace_back(nx, ny);
+                        }
+                    }
                 }
             }
         }
-        return INT_MAX;
+        return dist[r-1][c-1];
     }
 };
