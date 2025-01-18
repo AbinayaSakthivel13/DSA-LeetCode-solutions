581a74607337a5fd73890e2bf532b88f34da7ef6
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-18 22:49:17 +0530

    Time: 11 ms (87.62%) | Memory: 26.1 MB (41.91%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,33 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int dx[4] = {0, 0, 1, -1};
-const int dy[4] = {1, -1, 0, 0};
 class Solution {
 public:
     int minCost(vector<vector<int>>& grid) {
-        int r = grid.size(), c = grid[0].size();
-        vector<vector<int>> dist(r, vector<int>(c, INT_MAX));
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         deque<pair<int, int>> dq;
-        dq.emplace_front(0, 0);
-        dist[0][0] = 0;
+        int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
         
+        dq.push_front({0, 0});
+        dist[0][0] = 0;
+
         while (!dq.empty()) {
-            auto [x, y] = dq.front(); dq.pop_front();
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
-                    int cost = (i + 1 == grid[x][y]) ? 0 : 1;
-                    if (dist[x][y] + cost < dist[nx][ny]) {
-                        dist[nx][ny] = dist[x][y] + cost;
-                        if (cost == 0) {
-                            dq.emplace_front(nx, ny);
-                        } else {
-                            dq.emplace_back(nx, ny);
-                        }
-                    }
+            auto [x, y] = dq.front();
+            dq.pop_front();
+            int curDir = grid[x][y] - 1;
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir], ny = y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+
+                int cost = dist[x][y] + (dir == curDir ? 0 : 1);
+                if (cost < dist[nx][ny]) {
+                    dist[nx][ny] = cost;
+                    if (dir == curDir) dq.push_front({nx, ny});
+                    else dq.push_back({nx, ny});
                 }
             }
         }
-        return dist[r-1][c-1];
+
+        return dist[m - 1][n - 1];
     }
 };
