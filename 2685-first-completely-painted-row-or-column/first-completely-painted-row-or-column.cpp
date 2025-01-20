c1483861f1e454db3cfdab04acf69961fc9aed35
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-20 21:59:39 +0530

    Time: 17 ms (81.10%) | Memory: 134.4 MB (80.79%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         const int m=mat.size(), n=mat[0].size(), N=m*n;
-        vector<int> idx(N+1);
-        for(int i=0; i<N; i++)
-            idx[arr[i]]=i;
 
-        vector<int> to_i(N), to_j(N);
+        vector<int> to_i(N+1), to_j(N+1);
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                int pos=idx[mat[i][j]];
-                to_i[pos]=i;
-                to_j[pos]=j;
+                int x=mat[i][j];
+                to_i[x]=i;
+                to_j[x]=j;
             }
         }
         vector<int> row(m), col(n);
         for(int i=0; i<N; i++){
-            int pos=idx[arr[i]];
-            if (++row[to_i[pos]]==n || ++col[to_j[pos]]==m) return i;
+            int x=arr[i];
+            if (++row[to_i[x]]==n || ++col[to_j[x]]==m) return i;
         }
         return -1;
     }
