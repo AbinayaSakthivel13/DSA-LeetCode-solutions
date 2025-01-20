28cd4b08fe1a6df0640a407dc05cc359d19acb4b
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-20 21:58:50 +0530

    Time: 12 ms (83.84%) | Memory: 136.6 MB (79.27%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        unordered_map<int, int> mpr, mpc, mprc, mpcc;
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 0; j< mat[0].size(); ++j){
-                mpr[mat[i][j]] = i;     mpc[mat[i][j]] = j;
+        const int m=mat.size(), n=mat[0].size(), N=m*n;
+        vector<int> idx(N+1);
+        for(int i=0; i<N; i++)
+            idx[arr[i]]=i;
+
+        vector<int> to_i(N), to_j(N);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int pos=idx[mat[i][j]];
+                to_i[pos]=i;
+                to_j[pos]=j;
             }
         }
-        for(int i = 0; i < arr.size(); i++){
-            int n = arr[i];
-            mprc[mpr[n]]++;    mpcc[mpc[n]]++;
-            if(mprc[mpr[n]] == mat[0].size() || mpcc[mpc[n]] == mat.size()) return i;
+        vector<int> row(m), col(n);
+        for(int i=0; i<N; i++){
+            int pos=idx[arr[i]];
+            if (++row[to_i[pos]]==n || ++col[to_j[pos]]==m) return i;
         }
         return -1;
     }
