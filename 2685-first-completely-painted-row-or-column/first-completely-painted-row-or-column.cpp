2a9a78265bb3f108c9e38939859dbc5111f46bed
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-20 21:57:51 +0530

    Time: 115 ms (31.10%) | Memory: 171.1 MB (23.48%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int row = mat.size();
-        int col = mat[0].size();
-        unordered_map<int, pair<int, int>> pos;
-        
-        for(int i = 0; i < row; i++)
-            for(int j = 0; j < col; j++)
-                pos[mat[i][j]] = {i, j};
-        
-        vector<int> rCnt(row, 0);
-        vector<int> cCnt(col, 0);
-        
-        for(int i = 0; i < arr.size(); i++) {
-            auto [r, c] = pos[arr[i]];
-            rCnt[r]++;
-            cCnt[c]++;
-            if(rCnt[r] == col || cCnt[c] == row)
-                return i;
+        int rows = mat.size(), cols = mat[0].size();
+        unordered_map<int, pair<int, int>> positionMap;
+        vector<int> rowCount(rows, cols), colCount(cols, rows);
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                positionMap[mat[r][c]] = {r, c};
+            }
         }
-        return arr.size() - 1;
+        for (int idx = 0; idx < arr.size(); ++idx) {
+            int val = arr[idx];
+            auto [row, col] = positionMap[val];
+
+            if (--rowCount[row] == 0 || --colCount[col] == 0) {
+                return idx;
+            }
+        }
+        return -1;
     }
 };
