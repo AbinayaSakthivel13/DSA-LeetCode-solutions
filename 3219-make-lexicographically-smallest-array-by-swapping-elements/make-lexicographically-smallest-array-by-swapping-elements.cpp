2d5164fc1f14bf27ba6f0030f95a2e94a86fb958
Abinaya S <abinayasakthivel61@gmail.com>
2025-01-25 22:18:26 +0530

    Time: 179 ms (69.59%) | Memory: 184 MB (51.35%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    vector<int> lexicographicallySmallestArray(vector<int>& arr, int threshold) {
-        vector<pair<int, int>> valueIndexPairs;
-        int size = arr.size();
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        vector<pair<int, int>> b;
+        int n = nums.size();
 
-        for (int i = 0; i < size; ++i) {
-            valueIndexPairs.push_back({arr[i], i});
+        for (int i = 0; i < n; ++i)
+            b.push_back(make_pair(nums[i], i));
+
+        sort(b.begin(), b.end(), [](const auto& x, const auto& y) {
+            return x.first < y.first;
+        });
+        vector<vector<pair<int, int>>> c = {{b[0]}};
+
+        for (int i = 1; i < n; ++i) {
+            if (b[i].first - b[i - 1].first <= limit)
+                c.back().push_back(b[i]);
+            else
+                c.push_back({b[i]});
         }
 
-        sort(valueIndexPairs.begin(), valueIndexPairs.end());
+        for (const auto& t : c) {
+            vector<int> ind;
+            for (const auto& p : t)
+                ind.push_back(p.second);
+            sort(ind.begin(), ind.end());
 
-        vector<vector<pair<int, int>>> groupedPairs;
-        groupedPairs.push_back({valueIndexPairs[0]});
-
-        for (int i = 1; i < size; ++i) {
-            if (valueIndexPairs[i].first - valueIndexPairs[i - 1].first <= threshold) {
-                groupedPairs.back().push_back(valueIndexPairs[i]);
-            } else {
-                groupedPairs.push_back({valueIndexPairs[i]});
-            }
+            for (int i = 0; i < ind.size(); ++i)
+                nums[ind[i]] = t[i].first;
         }
-
-        for (const auto& group : groupedPairs) {
-            vector<int> indices;
-            for (const auto& [value, index] : group) {
-                indices.push_back(index);
-            }
-
-            sort(indices.begin(), indices.end());
-
-            for (size_t i = 0; i < indices.size(); ++i) {
-                arr[indices[i]] = group[i].first;
-            }
-        }
-
-        return arr;
+        return nums;
     }
 };
