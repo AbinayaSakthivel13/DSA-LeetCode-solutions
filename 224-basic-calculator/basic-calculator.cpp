01c4fa9ba02c90492713bebe7c2393437b126cd6
Abinaya S <abinayasakthivel61@gmail.com>
2025-06-20 21:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (31.41%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<pair<int, int>> stk;
+        int sign=1, sum=0;
+        for(int i=0;i<s.length();i++){
+            if(isdigit(s[i])){
+                long long num=0;
+                while(i<size(s) && isdigit(s[i])){
+                    num=num*10+s[i]-'0';
+                    i++;
+                }
+                i--;
+                sum+=num*sign;
+                sign=1;
+            }
+            else if(s[i]=='('){
+                stk.push({sum,sign});
+                sum=0;
+                sign=1;
+            }
+            else if(s[i]==')'){
+                sum=stk.top().first+sum*stk.top().second;
+                stk.pop();
+            }
+            else if(s[i]=='-'){
+                sign=-1*sign;
+            }
+        }
+        return sum;
+    }
+};
