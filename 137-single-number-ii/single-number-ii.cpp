ef87d7caebb4f0c5f2c6c618cc7040deb39fee3f
Abinaya S <abinayasakthivel61@gmail.com>
2025-06-25 23:21:42 +0530

    Time: 1 ms (45.13%) | Memory: 14 MB (18.18%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        int res=0;
+        for(int i=0;i<nums.size();i++)
+            freq[nums[i]]++;
+        for(auto num:freq){
+            if(num.second==1)
+                res=num.first;
+        }
+        return res;
+    }
+};
