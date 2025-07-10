defb2e3d0d158fb2facdd3dde7e09dcf77d2711a
Abinaya S <abinayasakthivel61@gmail.com>
2025-07-10 17:49:01 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (70.56%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,9 +3,8 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int exp_tot=n*(n+1)/2;
-        int act_tot=0;
         for(int num:nums)
-            act_tot+=num;
-        return exp_tot-act_tot;
+            exp_tot=exp_tot-num;
+        return exp_tot;
     }
 };
