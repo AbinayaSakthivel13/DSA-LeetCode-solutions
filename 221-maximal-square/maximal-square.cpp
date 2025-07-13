d069fc64a573bc04a6969589bc7b5d8374f413f6
Abinaya S <abinayasakthivel61@gmail.com>
2025-07-13 23:44:56 +0530

    Time: 12 ms (40.30%) | Memory: 31 MB (58.14%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        int maxSize=0;
-        vector<vector<int>> dp (rows, vector<int>(cols,0));
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
+        int r=matrix.size(), c=matrix[0].size();
+        int maxArea=0;
+        vector<vector<int>> dp(r, vector<int>(c,0));
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 if(matrix[i][j]=='1'){
                     if(i==0 || j==0)
                         dp[i][j]=1;
                     else
                         dp[i][j]=1+min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                    maxArea=max(maxArea, dp[i][j]);
                 }
-                maxSize=max(maxSize,dp[i][j]);
             }
         }
-        return maxSize*maxSize;
+        return maxArea*maxArea;
     }
 };
