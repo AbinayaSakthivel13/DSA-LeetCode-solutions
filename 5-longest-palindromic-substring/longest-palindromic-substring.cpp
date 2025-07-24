78d12583379765cb67e6fd665f063189d49322be
Abinaya S <abinayasakthivel61@gmail.com>
2025-07-24 17:38:34 +0530

    Time: 942 ms (8.41%) | Memory: 471.5 MB (5.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     string ans = "";
     void expand(string &s , int left ,int right)
@@ -19,5 +19,34 @@ public:
             expand(s , i , i+1);
         }
         return ans;
+    }
+};*/
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n <= 1) return s;
+
+        string result = s.substr(0, 1); // Default to first character
+
+        for (int i = 1; i < n; i++) {
+            // --- Odd-Length Palindrome ---
+            int l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                string palindrome = s.substr(l, r - l + 1);
+                if (palindrome.length() > result.length()) result = palindrome;
+                l--; r++;
+            }
+
+            // --- Even-Length Palindrome ---
+            l = i - 1; r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                string palindrome = s.substr(l, r - l + 1);
+                if (palindrome.length() > result.length()) result = palindrome;
+                l--; r++;
+            }
+        }
+
+        return result;
     }
 };
