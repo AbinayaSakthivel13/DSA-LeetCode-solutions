08997e0e2d2b43cb3656babe523f904eed09bccd
Abinaya S <abinayasakthivel61@gmail.com>
2025-07-24 17:37:14 +0530

    Time: 7 ms (87.35%) | Memory: 15 MB (51.27%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if (s.empty()) {
-            return "";
+    string ans = "";
+    void expand(string &s , int left ,int right)
+    {
+        while(left >= 0 &&  right < s.size())
+        {
+            if(s[left] != s[right])
+                break;
+            left--,right++;
         }
-
-        int start = 0;
-        int end = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            int odd = expandAroundCenter(s, i, i);
-            int even = expandAroundCenter(s, i, i + 1);
-            int max_len = max(odd, even);
-
-            if (max_len > end - start) {
-                start = i - (max_len - 1) / 2;
-                end = i + max_len / 2;
-            }
-        }
-
-        return s.substr(start, end - start + 1);        
+        if(ans.size() < right - left )
+            ans = s.substr(left + 1 , right - left - 1);
     }
-
-private:
-    int expandAroundCenter(string s, int left, int right) {
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            left--;
-            right++;
+    string longestPalindrome(string s) {
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            expand(s , i , i);
+            expand(s , i , i+1);
         }
-        return right - left - 1;
-    }    
+        return ans;
+    }
 };
