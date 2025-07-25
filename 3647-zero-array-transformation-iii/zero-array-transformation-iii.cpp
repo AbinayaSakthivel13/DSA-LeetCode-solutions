7d70a091b0aa9282f421c75541e290da22272203
Abinaya S <abinayasakthivel61@gmail.com>
2025-07-25 23:16:25 +0530

    Time: 53 ms (94.57%) | Memory: 242 MB (11.59%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
@@ -1,45 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries)
-    {
-        int n = nums.size();
-        int m = queries.size();
-
-        vector<int> workload(n + 1, 0);  // Difference array
-        sort(queries.begin(), queries.end());
-
-        priority_queue<int> available;
-        int qIndex = 0;
-
-        for (int time = 0; time < n; ++time)
-        {
-            if (time > 0)
-            {
-                workload[time] += workload[time - 1];
-            }
-
-            // Load queries that start at this time
-            while (qIndex < m && queries[qIndex][0] == time)
-            {
-                available.push(queries[qIndex][1]);
-                ++qIndex;
-            }
-
-            // Assign queries until we meet nums[time]
-            while (workload[time] < nums[time])
-            {
-                if (available.empty() || available.top() < time)
-                {
-                    return -1;
-                }
-
-                workload[time]++;
-                workload[available.top() + 1]--;
-                available.pop();
-            }
+    static int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        const int n=nums.size(), qz=queries.size();
+        vector<vector<int>> qEnd(n);
+        for(int i=0; i<qz; i++){
+            qEnd[queries[i][0]].push_back(queries[i][1]);
         }
-
-        return available.size(); // Remaining unused queries
+        priority_queue<int> pq;
+        vector<int> cntQ(n+1, 0);
+        int dec=0;
+        for(int i=0, j=0; i<n; i++){
+            const int x=nums[i];
+            dec+=cntQ[i];
+            for(int j: qEnd[i])
+                pq.push(j);
+            int k;
+            for(; x>dec && pq.size()>0 && (k=pq.top())>=i; dec++){
+                cntQ[k+1]--;
+                pq.pop();
+            }
+            if (x>dec) return -1;
+        }
+        return pq.size();
     }
 };
