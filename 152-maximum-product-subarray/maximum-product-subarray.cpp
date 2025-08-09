665bf2d5d2f5c7cf0884219084bbfd170cc5ba8d
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-09 10:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.38%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int prod1=nums[0], prod2=nums[0], res=nums[0];
@@ -7,6 +7,20 @@ public:
             prod2=min({nums[i], nums[i]*prod1, nums[i]*prod2});
             prod1=temp;
             res=max(res,prod1);
+        }
+        return res;
+    }
+};*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int res=*max_element(nums.begin(),nums.end());
+        int curMax=1, curMin=1;
+        for(int num:nums){
+            int temp=curMax*num;
+            curMax=max({temp, curMin*num, num});
+            curMin=min({temp, curMin*num, num});
+            res=max(res, curMax);
         }
         return res;
     }
