3a5d1c1132c12c0f01941294862e8a0e0df91c8d
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-11 10:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (99.57%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,12 +14,9 @@ public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<tuple<int, int, int>> nodes;  // (col, row, val)
         dfs(root, 0, 0, nodes);
-
         sort(nodes.begin(), nodes.end());
-
         vector<vector<int>> res;
         int prevCol = INT_MIN;
-
         for (auto [col, row, val] : nodes) {
             if (col != prevCol) {
                 res.push_back({});
@@ -27,10 +24,8 @@ public:
             }
             res.back().push_back(val);
         }
-
         return res;
     }
-
     void dfs(TreeNode* node, int row, int col, vector<tuple<int, int, int>>& nodes) {
         if (!node) return;
         nodes.emplace_back(col, row, node->val);
