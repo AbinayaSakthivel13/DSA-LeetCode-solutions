5446fdabccb383e168cdbf4fbbfab071fb424dad
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-22 22:40:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (90.74%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        int count=0;
-        for(int i=0;i<jewels.size();i++){
-            for(int j=0;j<stones.size();j++){
+        int count=0, i=0, j=0;
+        /*while(i<jewels.length()){
+            while(jewels[i]==stones[j] && j<stones.length())
+                count++;
+            j++;
+            i++;
+        }*/
+        for(int i=0;i<jewels.length();i++){
+            for(int j=0;j<stones.length();j++){
                 if(jewels[i]==stones[j])
                     count++;
             }
