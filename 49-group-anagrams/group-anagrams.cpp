470394567f481f363660df4e4bd61d4806067bc6
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-27 20:57:30 +0530

    Time: 12 ms (84.88%) | Memory: 26 MB (56.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> freq;
+        unordered_map<string, vector<string>> mp;
         for(string str:strs){
             string s=str;
-            sort(s.begin(),s.end());
-            freq[s].push_back(str);
+            sort(s.begin(), s.end());
+            mp[s].push_back(str);
         }
         vector<vector<string>> res;
-        for(auto itr:freq){
+        for(auto itr:mp)
             res.push_back(itr.second);
-        }
         return res;
     }
 };
