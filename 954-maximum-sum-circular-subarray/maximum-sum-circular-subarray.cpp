3faf8116f8f002964acd7096c4b3b249e00c4473
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-28 20:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (96.15%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int curMinSum=nums[0], maxSum=nums[0];
-        int curMaxSum=nums[0], minSum=nums[0];
-        int totalSum=nums[0];
+        int curMax=nums[0], curMin=nums[0], maxSum=nums[0], minSum=nums[0];
+        int total_sum=nums[0];
         for(int i=1;i<nums.size();i++){
-            curMaxSum=max(nums[i],curMaxSum+nums[i]);
-            maxSum=max(curMaxSum,maxSum);
-
-            curMinSum=min(nums[i],curMinSum+nums[i]);
-            minSum=min(curMinSum,minSum);
-
-            totalSum+=nums[i];
+            curMax=max(curMax+nums[i], nums[i]);
+            maxSum=max(maxSum,curMax);
+            curMin=min(curMin+nums[i], nums[i]);
+            minSum=min(minSum, curMin);
+            total_sum+=nums[i];
         }
-        int circularSum=totalSum-minSum;
-        if(circularSum==0)
+        int circular_sum=total_sum-minSum;
+        if(circular_sum==0)
             return maxSum;
-        return max(maxSum,circularSum);
+        return max(maxSum, circular_sum);
     }
 };
