669aa4b178e8b4b2619c07724baaec1eb92ee1fa
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-29 11:47:40 +0530

    Time: 27 ms (8.17%) | Memory: 74.5 MB (7.78%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,19 +12,27 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int rootIdx = 0;
-        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
-    }
-    
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
-        if (left > right) return NULL;
-        int pivot = left;  // find the root from inorder
-        while(inorder[pivot] != preorder[rootIdx]) pivot++;
-        
-        rootIdx++;
-        TreeNode* newNode = new TreeNode(inorder[pivot]);
-        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
-        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
-        return newNode;
+        if(preorder.empty() || inorder.empty())
+            return nullptr;
+
+        // Root node from preorder
+        TreeNode* root = new TreeNode(preorder[0]);
+
+        // Find root in inorder
+        auto pivot = find(inorder.begin(), inorder.end(), preorder[0]);
+
+        // Left and Right inorder
+        vector<int> leftInorder(inorder.begin(), pivot);
+        vector<int> rightInorder(pivot + 1, inorder.end());
+
+        // Left and Right preorder
+        vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + 1 + leftInorder.size());
+        vector<int> rightPreorder(preorder.begin() + 1 + leftInorder.size(), preorder.end());
+
+        // Recursive calls
+        root->left = buildTree(leftPreorder, leftInorder);
+        root->right = buildTree(rightPreorder, rightInorder);
+
+        return root;
     }
 };
