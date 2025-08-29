55b2a867d7f2db529a35f19fa10cc867210c5c7f
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-29 11:07:08 +0530

    Time: 12 ms (11.30%) | Memory: 25.3 MB (35.55%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char>> row(9);
-        vector<unordered_set<char>> col(9);
-        vector<unordered_set<char>> box(9);
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> cols(9);
+        vector<unordered_set<char>> boxes(9);
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j]=='.')
                     continue;
                 char num=board[i][j];
-                if(row[i].count(num))  return false;
-                row[i].insert(num);
-                if(col[j].count(num))  return false;
-                col[j].insert(num);
+                if(rows[i].count(num))  return false;
+                rows[i].insert(num);
+                if(cols[j].count(num))  return false;
+                cols[j].insert(num);
                 int boxIndex=(i/3)*3+j/3;
-                if(box[boxIndex].count(num))  return false;
-                box[boxIndex].insert(num);
+                if(boxes[boxIndex].count(num))   return false;
+                boxes[boxIndex].insert(num);
             }
         }
         return true;
