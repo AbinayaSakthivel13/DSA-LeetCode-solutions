5ce7a23c662554cbde1ed7acc2a300673d50594e
Abinaya S <abinayasakthivel61@gmail.com>
2025-08-30 09:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (80.35%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -7,11 +7,10 @@ public:
     bool dfs(const string& s, const vector<string>& wordDict, int ind, vector<int>& memo){
         if(ind==s.length())
             return true;
-        if(memo[ind]!=-1)
-            return memo[ind];
+        if(memo[ind] != -1) return memo[ind];
         for(int i=0;i<wordDict.size();i++){
             int len=wordDict[i].size();
-            if(ind+len<=s.size() && s.compare(ind, len, wordDict[i])==0){
+            if(ind+len<=s.length() && s.compare(ind, len, wordDict[i])==0){
                 if(dfs(s, wordDict, ind+len, memo))
                     return memo[ind]=true;
             }
@@ -19,24 +18,3 @@ public:
         return memo[ind]=false;
     }
 };
-/*class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        int n = s.size();
-        
-        vector<bool> dp(n + 1, false);
-        dp[0] = true; // base case: empty string is valid
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.count(s.substr(j, i - j))) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-
-        return dp[n];
-    }
-};*/
