eda243fcd7328761deed34e20ce3e070739e8504
Abinaya S <abinayasakthivel61@gmail.com>
2025-09-01 22:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (68.16%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        perm(nums,0,res);
+        return res;
+    }
+    void perm(vector<int>& nums, int i, vector<vector<int>>& res){
+        if(i==nums.size()){
+            res.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            perm(nums,i+1,res);
+            swap(nums[i],nums[j]);
+        }
+    }
+};
