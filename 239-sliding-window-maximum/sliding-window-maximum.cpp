48178c03bbc6bd01069b57845d252b33eaff0cb8
Abinaya S <abinayasakthivel61@gmail.com>
2025-09-14 18:03:33 +0530

    Time: 28 ms (42.15%) | Memory: 139.1 MB (59.79%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq;
@@ -14,5 +14,22 @@ public:
         }
 
         return ans;
+    }
+};*/
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> dq;
+        for(int i=0;i<nums.size();i++) {
+            while(!dq.empty() && nums[dq.back()]<=nums[i]) 
+                dq.pop_back();
+            dq.push_back(i);
+            if(!dq.empty() && dq.front()<=i-k) 
+                dq.pop_front();
+            if(i>=k-1) 
+                result.push_back(nums[dq.front()]);
+        }
+        return result;
     }
 };
